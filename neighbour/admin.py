cbe8930d22b013f82ea75392b3dcdd81378212a6
cheruiyotkelvin <ronohkelvin99@gmail.com>
2021-07-25 21:51:10 +0300

    register the database models

--- neighbour/admin.py
+++ neighbour/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+from .models import Neighbourhood, Location, Hospital
 # Register your models here.
+
+admin.site.register(Neighbourhood)
+admin.site.register(Location)
+admin.site.register(Hospital)
