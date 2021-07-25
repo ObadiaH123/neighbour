ac1fe1eec08101d93cf49a50d1d4996695036e9b
cheruiyotkelvin <ronohkelvin99@gmail.com>
2021-07-26 00:11:35 +0300

    update Healthecenter model

--- neighbour/models.py
+++ neighbour/models.py
@@ -41,7 +41,14 @@ class Location(models.Model):
     def save_location(self):
         self.save()
 
+class Healthcenter(models.Model):
+    name = models.CharField(max_length =30)
 
+    def __str__(self):
+        return self.name
+
+    def save_category(self):
+        self.save()
 
 
 
