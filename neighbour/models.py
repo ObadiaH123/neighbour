eda76a7857f07d4f760044074a515d3de49b27c2
cheruiyotkelvin <ronohkelvin99@gmail.com>
2021-07-25 21:43:42 +0300

    create the neighbourhood, Location and Hospital model

--- neighbour/models.py
+++ neighbour/models.py
@@ -1,3 +1,51 @@
 from django.db import models
 
-# Create your models here.
+from django.db import models
+import cloudinary
+from cloudinary.models import CloudinaryField 
+
+class Neighbourhood(models.Model):
+    name = models.CharField(max_length=55)
+    image = CloudinaryField('image') 
+    hospital =  models.ForeignKey('Hospital',on_delete=models.CASCADE)
+    location =  models.ForeignKey('Location',on_delete=models.CASCADE)
+
+    @classmethod
+    def search_by_category(cls,search_term):
+        image_category = cls.objects.filter(title__icontains=search_term)
+        return image_category
+
+
+    def __str__(self):
+        return self.name
+    class Meta:
+        ordering = ['name']
+
+    def save_neighbour(self):
+        self.save()
+
+    @classmethod
+    def get_image(cls):
+        image= cls.objects.get(pk=id)
+        return image
+
+class Location(models.Model):
+    name = models.CharField(max_length =30)
+
+    def __str__(self):
+        return self.name
+
+    def save_location(self):
+        self.save()
+
+class Hospital(models.Model):
+    name = models.CharField(max_length =30)
+
+    def __str__(self):
+        return self.name
+
+    def save_category(self):
+        self.save()
+
+
+
